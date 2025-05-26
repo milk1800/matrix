97852785780bb677544a1f519ed19958e4f959a7
Josh Bajorek <milk18001955@gmail.com>
2025-05-26 00:27:48 +0000

    I'm running a Next.js project inside Firebase Cloud Workstations and I changed my dev server port to 3002. Now I’m getting this CORS error in the terminal:
    vbnet
    Copy
    Edit
    ❗️Blocked cross-origin request from 3002-firebase-studio-1747526771127...cloudworkstations.dev. To allow this, configure "allowedDevOrigins" in next.config.ts.
    Update my next.config.ts file to include this new origin in the allowedDevOrigins array under the experimental section. Also make sure the other dev URLs (localhost and 8888-based workstation URLs) are still included.

--- next.config.ts
+++ next.config.ts
@@ -23,7 +23,8 @@ const nextConfig: NextConfig = {
       'http://localhost:8888',
       'https://8888b-firebase-studio-174752607117.zcl.dev',
       'https://8888b-firebase-studio-174752607117.zcl.cloudworkstations.dev',
-      'https://3001-firebase-studio-1747526771127.cluster-joak5ukfbnbyqspq@tewa33d24.cloudworkstations.dev'
+      'https://3001-firebase-studio-1747526771127.cluster-joak5ukfbnbyqspq@tewa33d24.cloudworkstations.dev',
+      'https://3002-firebase-studio-1747526771127.cluster-joak5ukfbnbyqspg4tewa33d24.cloudworkstations.dev'
     ]
   }
 };
